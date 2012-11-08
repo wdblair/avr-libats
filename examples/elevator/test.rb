f7a30e9c90b064ad7ff4f15365a6cf87f5d1851b
William Blair <wdblair@bu.edu>
2012-11-08 17:08:32 -0500

    Got a text based demo working.

--- examples/elevator/test.rb
+++ examples/elevator/test.rb
@@ -1,9 +1,60 @@
-#!/usr/bin/env rubyx
+#!/usr/bin/env ruby
 
+require 'rubygems'
 require 'serialport'
 
-sp = SerialPort.new "/dev/tty.usbmodemfd121", 9600
+semaphore = Mutex.new
 
-sp.print "u10\r"
+FLOORS = 10
 
-puts sp.readline("\r\n")
+curr = 0
+
+sp = SerialPort.new "/dev/tty.usbmodemfd121", 9600, 8 , 1, SerialPort::NONE
+
+#Serial Connection Resets the Chip.
+sleep 1.0
+
+a = Thread.new {
+  while true
+    #Go to a random floor
+    direction = (rand(2) == 0) ? 'u' : 'd'
+    floor = rand(FLOORS) + 1
+    out = "#{direction}#{floor}"
+    semaphore.synchronize {
+      puts "Need #{direction} at floor #{floor}"
+      sp.print "#{out}\r"
+      sp.flush
+    }
+    sleep (rand(5)+1)
+  end
+}
+
+while true
+  resp = ""
+  resp = sp.readline("\n")
+
+  if m = /^(f|o)([0-9]+).*/.match(resp)
+    if m[1] == 'f'
+      target = m[2].to_i
+      semaphore.synchronize {
+        puts "going to floor  #{m[2]}"
+        sp.print "a#{m[2]}\r"
+        sp.flush
+      }
+    elsif m[1] == 'o'
+      semaphore.synchronize {
+        (rand(2)+1).times do
+          flr = rand(FLOORS)+1
+          puts "requesting floor #{flr}"
+          sp.print "r#{flr}\r"
+          sp.flush
+        end
+      }
+      sleep 2.0
+      sp.print "c\r"
+    end
+  else
+    puts resp
+    raise
+  end
+end
