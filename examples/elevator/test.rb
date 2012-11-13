5807b2f9184ba07da917790b2612f98bd848364f
William Blair <wdblair@bu.edu>
2012-11-13 00:11:13 -0500

    Rewrote the PC side simulator.

--- examples/elevator/test.rb
+++ examples/elevator/test.rb
@@ -6,133 +6,193 @@ require 'rubygems'
 require 'serialport'
 require 'json'
 
-$semaphore = Mutex.new
-$FLOORS = 10
+FLOORS = 10
 
-$origin = Time.now.to_f * 1000.0
-
-def snapshot()
-  Time.now.to_f * 1000.0 - $origin
-end
-
-def char_of_direction(d)
-  d == 0 ? 'u' : 'd'
-end
-
-class Passenger
-  @@nextid = 0
-  
-  def initialize(floor, direction)
-    @id = @@nextid
-    @@nextid += 1
-    @start = floor
-    @direction = direction
-
-    #Not set until boarding the elevator.
-    @destination = 0
-
-    #Has exited the system
-    @arrived = false
-  end
-
-  def arrive
-    $semaphore.synchronize {
-      json.puts({:tag => "service", :id=> @id, :dir => direction, :flr => floor, :time => snapshot()}.to_json)
-      sp.print "#{out}\r"
-      sp.flush
-    }
-  end
-  
-
-  def board?(floor, direction)
-    @destination == 0 && @start == floor \
-      && @direction == direction
-  end
-
-  def board!
-    max_floor = @direction == 'u' ? $FLOORS : @start
-    lowest_floor = @direction == 'u' ? @start : 1
-    flr = rand(max_floor)+lowest_floor
-    json.puts({:tag => "request", :id => id, :flr => flr, :time => snapshot()}.to_json)
-    $semaphore.synchronize {
-      sp.print "r#{flr}\r"
-      sp.flush
-    }
-    sleep 1.0
-  end
-  
-  def leave?(floor)
-    @destination == floor
-  end
-  
-  def leave!
-    json.puts({:tag=>"exit", :id=>@id, :floor=>@destination}.to_json)
-  end
-end
-
-curr = 1
-
-sp = SerialPort.new "/dev/tty.usbmodemfd121", 9600
+$sp = SerialPort.new "/dev/tty.usbmodemfd121", 9600
 
 #Opening the Serial Port resets
 #the MCU, give it a second...
 sleep 1.0
 
+def char_of_direction(d)
+  d == 0 ? 'u' : 'd'
+end
+
+class Elevator
+  def initialize()
+    @semaphore = Mutex.new
+    @sp = SerialPort.new "/dev/tty.usbmodemfd121", 9600
+    @curr = 1
+    @direction = 'u'
+    sleep 1.0
+    @origin = Time.now.to_f * 1000.0
+  end
+  
+  #Send a service request from someone
+  #waiting for the elevator.
+  def service(id, direction, flr)
+    out = "#{direction}#{flr}\r"
+    send_message(out)
+    js = {:id=> id, :dir => direction, :flr => flr}
+    publish_event("service", js)
+  end
+
+  #Send a request from a passenger
+  #inside the elevator to the controller
+  def request(id, floor)
+    send_message("r#{floor}\r")
+    js = {:id => id, :flr => floor}
+    publish_event("request", js)
+  end
+  
+  def exit(id, floor)
+    js = {:id => id, :flr => floor}
+    publish_event("exit", js)
+  end
+  
+  #Record doors opening
+  def open(dir)
+    @direction = char_of_direction(dir)
+    js = {:direction=>@direction}
+    publish_event("open", js)
+  end
+
+  #Close the doors
+  def close()
+    send_message("c\r")
+    publish_event("close", {})
+  end
+  
+  #Move to a specific floor.
+  def move(to)
+    js = {:from => @curr}
+    publish_event("move", js)
+
+    #Actually move to the floor
+    diff = (@curr - to).abs
+    sleep diff
+    send_message("a#{to}\r")
+    @curr = to
+    js = {:flr => @curr}
+    publish_event("arrive", js)
+  end
+
+  #Wait for a message from the device
+  def wait()
+    resp = @sp.readline("\n")
+    
+    if m = /^(f|o)([0-9]+).*/.match(resp)
+      return m
+    end
+    
+    puts resp
+    raise
+  end
+ 
+  def floor()
+    @curr
+  end
+  
+  def direction()
+    @direction
+  end
+
+  #Send a string to the device
+  def send_message(msg)
+    @semaphore.synchronize {
+      @sp.print(msg)
+      @sp.flush()
+    }
+  end
+
+  #Log a message to the simulator
+  def publish_event(tag, msg)
+    msg[:tag] = tag
+    msg[:time] = snapshot()
+    puts msg.to_json()
+  end
+
+  def snapshot()
+    Time.now.to_f * 1000.0 - @origin
+  end
+end
+
+class Passenger
+  @@nextid = 0
+
+  def initialize()
+    #Fetch a random direction and floor.
+    direction = char_of_direction(rand(2))
+    floor = rand(FLOORS) + 1
+    @id = @@nextid
+    @@nextid += 1
+    @start = floor
+    @direction = direction
+    
+    #Not set until boarding the elevator.
+    @destination = 0
+  end
+  
+  def arrive(elevator)
+    elevator.service(@id, @direction, @start)
+  end
+  
+  def board(elevator)
+    if @destination == 0 && @start == elevator.floor() \
+      && @direction == elevator.direction()
+      
+      max_floor = @direction == 'u' ? FLOORS : @start
+      lowest_floor = @direction == 'u' ? @start : 1
+      dist = max_floor - lowest_floor
+      flr = rand(dist)+lowest_floor
+      if flr > FLOORS
+        flr = rand(FLOORS)+1
+      end
+      @destination = flr
+      elevator.request(@id, flr)
+      sleep 1.0
+    end
+  end
+
+  def leave(elevator)
+    res = @destination == elevator.floor()
+    if res
+      elevator.exit(@id, @destination)
+    end
+    res
+  end
+  
+end
+
 users = []
 
-json = File.open("output.json", "w+")
+elevator = Elevator.new()
 
 a = Thread.new {
   10.times do
     #Go to a random floor
-    direction = char_of_direction(rand(2))
-    floor = rand($FLOORS) + 1
-    out = "#{direction}#{floor}"
-    $semaphore.synchronize {
-      pass = Passenger.new(floor, direction)
-      pass.arrive()
-      users.push(pass)
-    }
+    pass = Passenger.new()
+    pass.arrive(elevator)
+    users.push(pass)
     sleep (rand(5)+1)
   end
-  stderr.puts "done"
 }
 
 while true
-  resp = ""
-  resp = sp.readline("\n")
-
-  if m = /^(f|o)([0-9]+).*/.match(resp)
-    if m[1] == 'f'
-      target = m[2].to_i
-      diff = (curr - target).abs
-      json.puts({:tag => "move", :from => curr, :time => snapshot()}.to_json)
-      sleep diff
-      $semaphore.synchronize {
-        curr = target
-        json.puts({:tag => "arrive", :flr => curr, :time => snapshot()}.to_json)
-        sp.print "a#{m[2]}\r"
-        sp.flush
-      }
-    elsif m[1] == 'o'
-      json.puts({:tag => "open", :time => snapshot()}.to_json)
-      users.delete_if { |u|
-        res = u.leave?
-        u.leave! if res
-        res
-      }
-      users.each { |u|
-        u.board! if u.board?
-      }
-      $semaphore.synchronize {
-        json.puts({:tag => "close", :time => snapshot()}.to_json)
-        sp.print "c\r"
-        sp.flush
-      }
-    end
-  else
-    puts resp
-    raise
+  cmd = elevator.wait()
+  case cmd[1]
+  when 'f' then
+    target = cmd[2].to_i
+    elevator.move(target)
+  when 'o' then
+    elevator.open(cmd[2].to_i)
+    users.delete_if { |u|
+      u.leave(elevator)
+    }
+    users.each { |u|
+      u.board(elevator)
+    }
+    elevator.close()
   end
-  json.flush
+  STDOUT.flush()
 end
